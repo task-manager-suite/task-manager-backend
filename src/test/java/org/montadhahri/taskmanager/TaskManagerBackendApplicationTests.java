c76300586602f8f2b83cba7d55f01b96ec3a1380
Montassar Dhahri <mdh>
2025-06-21 17:40:52 +0100

    Initialize Spring Boot project with H2/flyway configuration

---
+++ src/test/java/org/montadhahri/taskmanager/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.montadhahri.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
